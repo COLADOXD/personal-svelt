22cc51e3ed6b18f60d9fb235d99004308eaeb574
MateoChacon <xdmateo90@gmail.com>
2024-01-18 18:57:56 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+  <p class="text-6xl bg-green-500">hi</p>
+</main>
