69fe6ddd821dbd04b03887ea4a9f6ee8b3a0778d
MateoChacon <xdmateo90@gmail.com>
2024-01-18 19:13:43 -0500

    Install skeleton

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
-  import Counter from "./lib/Counter.svelte";
+  import { Avatar } from "@skeletonlabs/skeleton";
 </script>
 
 <main>
-  <p class="text-6xl bg-green-500">hi</p>
+  <Avatar
+    src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop"
+    width="w-32"
+    rounded="rounded-full"
+  />
 </main>
