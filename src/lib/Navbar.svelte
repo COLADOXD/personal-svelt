c4bdd5dd5da808f72bf51b7907cd67e38cb2f821
MateoChacon <xdmateo90@gmail.com>
2024-02-01 12:16:29 -0500

    responsive Nav

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -5,6 +5,22 @@
 
   export let showMenu: boolean;
   export let handleMenu: Function;
+  let widthWindow =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+
+  const handleResizing = () => {
+    widthWindow =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
+    if (widthWindow > 900) {
+      handleMenu(false);
+    }
+  };
+
+  window.addEventListener("resize", handleResizing);
 </script>
 
 <nav class="h-16 flex justify-between items-center m-8">
@@ -12,10 +28,15 @@
     <Avatar src={AvatarBg} width="w-20" rounded="rounded-full" />
     <p class="ml-4 text-xl">Mateo Chacon</p>
   </div>
-  <button on:click={() => ((showMenu = !showMenu), handleMenu(showMenu))}
-    ><Icon icon="fa6-solid:align-justify"></Icon></button
-  >
+  {#if widthWindow < 900}
+    <button on:click={() => ((showMenu = !showMenu), handleMenu(showMenu))}>
+      <Icon icon="fa6-solid:align-justify"></Icon>
+    </button>
+  {/if}
+  {#if widthWindow > 900}
+    <div class="flex">
+      <p class="my-4 text-xl font-semibold mr-8">All the projects</p>
+      <p class="my-4 text-xl font-semibold">About</p>
+    </div>
+  {/if}
 </nav>
-{#if showMenu}
-  <div class="w-full h-full"></div>
-{/if}
