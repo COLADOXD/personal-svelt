c83882a48b20d989c9d9d6ff21753486cc7bd22c
MateoChacon <xdmateo90@gmail.com>
2024-02-01 11:25:57 -0500

    add Menu

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
   import { Avatar } from "@skeletonlabs/skeleton";
-  import AvatarBg from "../assets/itachi.jpeg";
+  import Icon from "@iconify/svelte";
+  import AvatarBg from "../assets/descarga.jpeg";
+
+  export let showMenu: boolean;
+  export let handleMenu: Function;
 </script>
 
-<nav class="h-20 flex justify-between items-center m-10">
+<nav class="h-16 flex justify-between items-center m-8">
   <div class="flex items-center">
     <Avatar src={AvatarBg} width="w-20" rounded="rounded-full" />
-    <p class="ml-4 text-xl">Mateo Sebastian Chacon</p>
+    <p class="ml-4 text-xl">Mateo Chacon</p>
   </div>
+  <button on:click={() => ((showMenu = !showMenu), handleMenu(showMenu))}
+    ><Icon icon="fa6-solid:align-justify"></Icon></button
+  >
 </nav>
+{#if showMenu}
+  <div class="w-full h-full"></div>
+{/if}
